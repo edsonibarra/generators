67c4e645efe543af4f9fc3c7d99aead7cef15251
edson ibarra <edsonibarra97@gmail.com>
2023-01-02 22:09:09 -0600

    docstrings

--- generator.py
+++ generator.py
@@ -1,4 +1,9 @@
 def inclusive_range(start, end):
+    """
+    It will generate a new value till reaches the end
+    Values will be generated in a range start and end (inclusive).
+    Similar to range() built-in funciton, but this is inclusive in the end param.
+    """
     for i in range(start, end + 1):
         yield i
 
