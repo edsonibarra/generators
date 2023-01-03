369ae65e84fcda363aa2028cfe7cae449d9e32eb
edson ibarra <edsonibarra97@gmail.com>
2023-01-02 22:13:53 -0600

    main function created

--- generator.py
+++ generator.py
@@ -1,6 +1,5 @@
 def inclusive_range(start, end):
-    """
-    It will generate a new value till reaches the end
+    """ It will generate a new value till reaches the end
     Values will be generated in a range start and end (inclusive).
     Similar to range() built-in funciton, but this is inclusive in the end param.
     """
@@ -8,5 +7,10 @@ def inclusive_range(start, end):
         yield i
 
 
-for i in inclusive_range(1, 3):
-    print(i)
+def main():
+    for i in inclusive_range(1, 3):
+        print(i)
+
+
+if __name__ == '__main__':
+    main()
